3f479f1477d737aa01b3d9a05a179e0bd713dd47
Facts-Solver <140691347+Facts-Solver@users.noreply.github.com>
2023-10-02 14:58:53 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ window.onscroll = () => {
 	let navbar = document.querySelector("nav")
 		navbar.classList.toggle("scroll",window.scrollY > 185)
 }
+const observer = new IntersectionObserver((entry)=>{
+	entry.forEach((entry)=>{
+		if(entry.isIntersecting){
+			entry.target.classList.remove("hide")
+			entry.target.classList.add("show")
+		}
+		else{
+			entry.target.classList.add("hide")
+			entry.target.classList.remove("show")
+		}
+	})
+})
+let hidden = document.querySelectorAll(".hide")
+hidden.forEach((entry)=>{
+	observer.observe(entry)
+})
